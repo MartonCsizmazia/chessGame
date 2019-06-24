b260ed767959f14cea72bea9fc4701762f8bc4e7
Daniel Kiraly <danielkiraly@ymail.com>
2019-06-24 14:00:51 +0200

    Implement drag&drop

--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,11 @@
 function main() {
     let trialCell = document.getElementById('icon-0:0');
     trialCell.setAttribute('class', 'fas fa-chess-king');
+
+    let containers = document.querySelectorAll('.game-cell');
+    let containersArray = Array.from(containers);
+    dragula(containersArray);
+
 }
 
 main();
