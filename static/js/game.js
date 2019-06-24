dc71fd8c70eec44d5c610029d4702388395ab4da
Daniel Kiraly <danielkiraly@ymail.com>
2019-06-24 15:10:54 +0200

    Fix drag&drop function

--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,4 @@
 function main() {
-    let trialCell = document.getElementById('icon-0:0');
-    trialCell.setAttribute('class', 'fas fa-chess-king');
 
     let containers = document.querySelectorAll('.game-cell');
     let containersArray = Array.from(containers);
