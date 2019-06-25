32b7879dcf23ba27fd9538e85ee5e0aa68d6b4e1
David Korpos <korposdavid@gmail.com>
2019-06-25 10:19:08 +0200

    add winCheck

--- static/js/game.js
+++ static/js/game.js
@@ -6,4 +6,16 @@ function main() {
 
 }
 
+function winCheck() {
+    let gameCells = document.getElementsByClassName('game-cell');
+    let kingCount = 0;
+    for (let cell of gameCells) {
+        let actualAttribute = cell.getElementsByTagName('i')[0].getAttribute('class');
+        if (actualAttribute && 'fas fa-chess-king' === actualAttribute.slice(0,17)) {
+            kingCount ++;
+        }
+    }
+    return (kingCount === 2);
+}
+
 main();
